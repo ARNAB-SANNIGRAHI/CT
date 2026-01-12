d2f9bcee211505f249b082915030c78d09ccb01d
Arnab Sannigrahi <120069871+ARNAB-SANNIGRAHI@users.noreply.github.com>
2026-01-12 10:19:17 +0530

    Update Assignment18_Scanner_Method_HappyNumber.java

--- core_java/Assignment18_Scanner_Method_HappyNumber.java
+++ core_java/Assignment18_Scanner_Method_HappyNumber.java
@@ -1,8 +1,6 @@
 package core_java;
 import java.util.Scanner;
-
 public class Assignment18_Scanner_Method_HappyNumber {
-
 	    private static int getNext(int n) {
 	        int sum = 0;
 	        while (n > 0) {
@@ -12,21 +10,17 @@ public class Assignment18_Scanner_Method_HappyNumber {
 	        }
 	        return sum;
 	    }
-
 	    public static boolean isHappy(int n) {
 	        int slow = n;
 	        int fast = getNext(n);
-
 	        while (fast != 1 && slow != fast) {
 	            slow = getNext(slow);
 	            fast = getNext(getNext(fast));
 	        }
 	        return fast == 1;
 	    }
-
 	    public static void main(String[] args) {
 	        Scanner sc = new Scanner(System.in);
-
 	        System.out.print("Enter a number: ");
 	        int number = sc.nextInt();
 
@@ -35,7 +29,6 @@ public class Assignment18_Scanner_Method_HappyNumber {
 	        } else {
 	            System.out.println(number + " is not a Happy Number");
 	        }
-
 	        sc.close();
 }
 }
