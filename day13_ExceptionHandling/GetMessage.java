0ba4d73e63ecb3f036c67c41581fd3e98dc633ca
Arnab Sannigrahi <120069871+ARNAB-SANNIGRAHI@users.noreply.github.com>
2026-01-24 18:50:49 +0530

    Add files via upload

---
+++ day13_ExceptionHandling/GetMessage.java
@@ -0,0 +1,15 @@
+package day13_ExceptionHandling;
+
+public class GetMessage {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		try {
+			System.out.println(7 / 0);
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		System.out.println("bye");
+	}
+}
